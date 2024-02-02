b83797e73dbdc9bbace0112f108adaa1c1f236f1
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-02 14:13:16 +0200

    Update notFound.jsx

--- src/components/errors/notFound.jsx
+++ src/components/errors/notFound.jsx
@@ -1,22 +1,23 @@
-import { useParams, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import s from './nf.module.css';
 
-export const NF404 = ({ error = 'Not Found' }) => {
-  console.log('useParams: ', useParams());
+export const NF404 = ({ error = 'Page Not Found' }) => {
   console.log('useLocation: ', useLocation());
+  const location = useLocation();
   return (
-    <>
+    <article className={s.article}>
       <div className="container">
-        <article>
-          <h1>ERRORS</h1>
-          <section>
-            <h2>{error}</h2>
-						{/* <p>useParams: {useParams()}</p>
-						<p>useLocation: {useLocation()}</p> */}
-            <div className={s.thumb}></div>
-          </section>
-        </article>
+        <h1>ERRORS</h1>
+        {/* <div className={s.container}> */}
+				<div className={s.stub}>
+
+				</div>
+        <section className={s.section}>
+          <p className={s.info}>Сторінка: {location.pathname}</p>
+          <h1>{error}</h1>
+          <div className={s.thumb}></div>
+        </section>
       </div>
-    </>
+    </article>
   );
 };
