322fd8ee2139b727ef7994fba554a37ef6eeb2ae
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-07 21:04:58 +0200

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,39 +1,43 @@
-import { Link } from "react-router-dom";
-import s from "./footer.module.css";
+import { Link } from 'react-router-dom';
+import s from './footer.module.css';
 
 export const Footer = () => {
   return (
     <footer className={s.wrapper}>
       <div className="container">
         <div className={s.flex}>
-          <section className={s.address}>
+          <div className={s.address}>
             <h2 className={s.title}>наші контакти</h2>
             <div className={s.content}>
-							<p className={s.text_left}>наша адреса</p>
-              <p className={s.text}> 12<sup className={s.downIndex}>та</sup> Поздовжня, 63, Миколаїв</p>
+              <p className={s.text_left}>наша адреса</p>
+              <p className={s.text}>
+                {' '}
+                12<sup className={s.downIndex}>та</sup> Поздовжня, 63, Миколаїв
+              </p>
               {/* <p className={s.text}>54028, м. Миколаїв.</p> */}
-							{/* <p className={s.text}>абонентська скринька : 174</p> */}
-							<hr/>
-							<p className={s.text_left}>наші засоби зв'язку</p>
+              {/* <p className={s.text}>абонентська скринька : 174</p> */}
+              <hr />
+              <p className={s.text_left}>наші засоби зв'язку</p>
               <p className={s.text}>тел.: +380(512)71-16-90</p>
               <p className={s.text}>e-mail: sales@polus-ug.mk.ua</p>
             </div>
-          </section>
-          <section className={s.socials}>
+          </div>
+          <div className={s.socials}>
             <h2 className={s.title}>Приєднуйтесь до нас</h2>
             <div className={s.content}>
-							<hr/><br/>
+              <hr />
+              <br />
               <ul className={s.flexSocial}>
                 <li>
-                  <Link to="https://about.instagram.com/">
+                  <Link className={s.socLink} to="https://about.instagram.com/">
                     <svg className={s.instagram}>
-										<use href="./icons/defs.svg#s-instagram"></use>
+                      <use href='./icons/defs.svg#s-instagram'></use>
                     </svg>
                   </Link>
                 </li>
 
                 <li>
-                  <Link to="https://twitter.com/">
+                  <Link className={s.socLink} to="https://twitter.com/">
                     <svg className={s.twitter}>
                       <use href="./icons/defs.svg#s-twitter"></use>
                     </svg>
@@ -41,7 +45,7 @@ export const Footer = () => {
                 </li>
 
                 <li>
-                  <Link to="https://www.facebook.com/">
+                  <Link className={s.socLink} to="https://www.facebook.com/">
                     <svg className={s.facebook}>
                       <use href="./icons/defs.svg#s-facebook"></use>
                     </svg>
@@ -49,7 +53,7 @@ export const Footer = () => {
                 </li>
 
                 <li>
-                  <Link to="https://www.linkedin.com/">
+                  <Link className={s.socLink} to="https://www.linkedin.com/">
                     <svg className={s.linkedin}>
                       <use href="./icons/defs.svg#s-linkedin"></use>
                     </svg>
@@ -57,29 +61,30 @@ export const Footer = () => {
                 </li>
               </ul>
             </div>
-          </section>
-          <section className={s.subscription}>
+          </div>
+          <div className={s.subscription}>
             <h2 className={s.title}>subscription</h2>
             <form className={s.content}>
-						<div className={s.flexSub}>
-
-              <input
-                className={s.name}
-                type="text"
-                name="name"
-                placeholder="ваше имя"
-              />
-              <input
-                className={s.email}
-                type="email"
-                name="email"
-                placeholder="info@devstudio.com"
-              />
-              <button className={s.btnSubmit} type="submit">подписаться</button>
-						</div>
+              <div className={s.flexSub}>
+                <input
+                  className={s.name}
+                  type="text"
+                  name="name"
+                  placeholder="ваше имя"
+                />
+                <input
+                  className={s.email}
+                  type="email"
+                  name="email"
+                  placeholder="info@devstudio.com"
+                />
+                <button className={s.btnSubmit} type="submit">
+                  подписаться
+                </button>
+              </div>
               <p></p>
             </form>
-          </section>
+          </div>
         </div>
       </div>
     </footer>
