ca295be05be08ca2601174ff08d0eacdece31f15
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-07 21:03:55 +0200

    Update logo.jsx

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,11 +1,12 @@
 import s from './logo.module.css';
 
-export const Logo = ({theme="light"}) => {
+export const Logo = ({ theme = 'light' }) => {
   return (
     <div className={s.wrapper}>
-      <div className="container">
-        <h1 className={theme === "light"? s.h_light:s.h_dark}>Logo</h1>
-        <div className={s.flex}>Logo<span>type</span></div>
+      <div className={s.flex}>
+      <span className={theme === 'light' ? s.h_light : s.h_dark}>Полюс</span>
+			<a href="/" target="_blank"><img src="https://images2.imgbox.com/a5/10/Va53IWL6_o.png" alt="pinguin192"/></a>
+        <span>Юг</span>
       </div>
     </div>
   );
